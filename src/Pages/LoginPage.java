5394ff2632a39bacfb779e266ce56dafa92d44aa
Punam solanki <solankipunam06@gmai.com>
2025-02-09 16:40:42 +0530

    add/delete/modified

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -6,6 +6,9 @@ public class LoginPage {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello, this my login page");
+		String username= "Admin";
+		String passworld= "Admin";
+		System.out.println("Logged in....");
 
 	}
 
---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Search page");
+
+	}
+
+}
